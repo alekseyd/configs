ee8559f03cc1328bf23fc292e7b2994742fc6590
Aleksey Dashevsky <aleksey@raven.io>
2023-08-21 12:27:26 +0300

    Make setup.sh agnostic to its location

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 # Specify the preferences directory
 script_dir=$(cd "$(dirname "$0")" && pwd)
-defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string "${script_dir}/configs/"
+defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string "${script_dir}"
 # Tell iTerm2 to use the custom preferences in the directory
 defaults write com.googlecode.iterm2.plist LoadPrefsFromCustomFolder -bool true
